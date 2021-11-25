81d635a03f4a14936d06266b7f0be656d1ff76cc
oldboyee <fuukingy@gmail.com>
2021-11-25 21:09:36 +0200

    ng

---
+++ module12/ListUsers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace module12
+{
+    public class ListUsers
+    {
+        private List<Users> usersList = new List<Users>();
+
+
+        public List<Users> GetUsers()
+        {
+            usersList.Add(new Users { Login = "strit", Name = "Виктор", IsPremium = false });
+            usersList.Add(new Users { Login = "GEbHS", Name = "Ирина", IsPremium = false });
+            usersList.Add(new Users { Login = "hzveW", Name = "Михаил", IsPremium = true });
+            usersList.Add(new Users { Login = "stcsa", Name = "Виктория", IsPremium = true });
+
+
+            return usersList;
+        }
+    }
+}
---
+++ module12/Users.cs
@@ -0,0 +1,9 @@
+using System;
+namespace module12
+{
+	public class Users { 
+		public string Login { get; set; }
+		public string Name { get; set; }
+		public bool IsPremium { get; set; }
+	}
+}
